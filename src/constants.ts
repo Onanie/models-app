421213c4759e6621b8551d4045008af122672b7e
OnanieM <Ximhungwe@1281>
2022-01-28 11:26:49 +0200

    removed api_key

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://api.up2tom.com/v3/';
-export const API_KEY = '9307bfd5fa011428ff198bb37547f979';
+export const API_KEY = '***';
 export const MODEL_KEY = '58d3bcf97c6b1644db73ad12';
